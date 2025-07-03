36c1a1139196ffc22627a60aa012e2c87b7bbbf6
Coralesk1 <guscoraleski2006@gmail.com>
2025-07-03 12:36:36 -0300

    first commit

---
+++ src/main/java/com/Coralesk1/Calculator_Spring_boot/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.Coralesk1.Calculator_Spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
